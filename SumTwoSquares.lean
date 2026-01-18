7a2f74e7f56ef6bd3863183fde2829edf881c7a0
Jack Westbrook <jackswestbrook@gmail.com>
2026-01-18 13:20:34 +0000

    Initial commit

---
+++ SumTwoSquares.lean
@@ -0,0 +1,1 @@
+import SumTwoSquares.Basic
---
+++ SumTwoSquares/MainTheorem.lean
@@ -0,0 +1,4 @@
+import Mathlib
+
+theorem prime_sum_two_squares (p: ℕ ) [Fact p.Prime] : (∃ a b :ℤ, p = a^2 + b^2) ↔ p%4 = 1  /- ↔ p splits in ℤ[i] -/  :=
+sorry
